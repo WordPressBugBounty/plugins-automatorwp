585a89e510be1f1a23c27186681a4f7361c13afa
Sean Murphy <sean@iamseanmurphy.com>
2025-01-09 14:17:16 +0000

    5.1.2

--- automatorwp/automatorwp.php
+++ automatorwp/automatorwp.php
@@ -3,7 +3,7 @@
  * Plugin Name:     	AutomatorWP
  * Plugin URI:      	https://automatorwp.com
  * Description:     	Connect your WordPress plugins together and create automated workflows with no code!
- * Version:         	5.1.1
+ * Version:         	5.1.2
  * Author:          	AutomatorWP
  * Author URI:      	https://automatorwp.com/
  * Text Domain:     	automatorwp
@@ -120,7 +120,7 @@ final class AutomatorWP {
     private function constants() {
 
         // Plugin version
-        define( 'AUTOMATORWP_VER', '5.1.1' );
+        define( 'AUTOMATORWP_VER', '5.1.2' );
 
         // Plugin file
         define( 'AUTOMATORWP_FILE', __FILE__ );
