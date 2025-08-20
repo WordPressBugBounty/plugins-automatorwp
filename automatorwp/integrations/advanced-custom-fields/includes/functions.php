1ccd6ecebfd822e8932bb38a2bceb61387266fc6
Sean Murphy <sean@iamseanmurphy.com>
2025-08-20 08:18:33 +0000

    5.3.5

--- automatorwp/integrations/advanced-custom-fields/includes/functions.php
+++ automatorwp/integrations/advanced-custom-fields/includes/functions.php
@@ -15,63 +15,16 @@ if( !defined( 'ABSPATH' ) ) exit;
  *
  * @return array
  */
-function automatorwp_advanced_custom_fields_options_cb_fields_posts() {
+function automatorwp_advanced_custom_fields_options_cb_fields_posts( ) {
 
     $options = array(
         'any' => __( 'any field', 'automatorwp-advanced-custom-fields' ),
     );
     
     // Get all post types
-    $all_post_types = acf_get_post_types( );
-
-    foreach ( $all_post_types as $post_type ) {
-
-        $args_acf = array(
-            'post_type' => $post_type,
-        );
-
-        // Get groups related to posts
-        $all_post_groups = acf_get_field_groups( $args_acf );
-        
-        foreach( $all_post_groups as $group ) {
-        
-            // Get fields from group
-            $all_acf_fields = acf_get_fields( $group['ID'] );
-
-            foreach ( $all_acf_fields as $acf_fields ){
-
-                $options[$acf_fields['name']] = $acf_fields['label'];
-            }
-
-        }
-
-    }
+    $all_post_types = automatorwp_advanced_custom_fields_get_post_types_field_groups( 'post' );
     
-    return $options;
-
-}
-
-/**
- * Get acf fields related to users
- *
- * @since 1.0.0
- *
- * @return array
- */
-function automatorwp_advanced_custom_fields_options_cb_fields_users() {
-
-    $options = array(
-        'any' => __( 'any field', 'automatorwp' ),
-    );
-    
-    $args = array(
-        'user_form' => 'all',
-    );
-    
-    // Get groups related to users
-    $all_user_groups = acf_get_field_groups( $args );
-    
-    foreach( $all_user_groups as $group ) {
+    foreach( $all_post_types as $group ) {
     
         // Get fields from group
         $all_acf_fields = acf_get_fields( $group['ID'] );
@@ -84,5 +37,61 @@ function automatorwp_advanced_custom_fields_options_cb_fields_users() {
     }
     
     return $options;
+
+}
+
+/**
+* Get all post field groups 
+*
+* @return array 
+*/
+function automatorwp_advanced_custom_fields_get_post_types_field_groups( $post_type ) {
+
+	if ( ! function_exists( 'acf_get_field_groups' ) ) {
+		return array();
+	}
+
+	$groups_post = array();
+
+    switch ( $post_type ){
+        case 'post':
+            $groups_types = array(
+                'post_type',
+                'post_template',
+                'post_status',
+                'post_format',
+                'post_category',
+                'post_taxonomy',
+                'post',
+            );
+            break;
+    }
+
+	$groups = acf_get_field_groups();
+
+	foreach ( $groups as $group ) {
+
+		if ( ! empty( $group['location'] ) ) {
+
+			foreach ( $group['location'] as $locations ) {
+
+				foreach ( $locations as $location ) {
+
+                        if ( ! in_array( $location['param'], $groups_types )) {
+                            continue;
+    
+                        } else {
+                            $groups_post[] = $group;
+                        }
+
+				}
+
+			}
+
+		}
+        
+	}
+
+	return $groups_post;
 
 }
--- automatorwp/readme.txt
+++ automatorwp/readme.txt
@@ -3,7 +3,7 @@ Contributors: automatorwp, rubengc, eneribs, dioni00, tinocalvo, pacogon, flaber
 Tags: automator, automation, zapier, webhooks, marketing
 Requires at least: 4.4
 Tested up to: 6.8
-Stable tag: 5.3.4
+Stable tag: 5.3.5
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -334,6 +334,11 @@ Yes, AutomatorWP is stored in the official WordPress plugins repository where yo
 No, We're unable to provide custom development services, as our focus is developing the core AutomatorWP plugin, and the official AutomatorWP add-ons.
 
 == Changelog ==
+
+= 5.3.5 =
+
+* **Improvements**
+* ACF: Improved the selection of fields.
 
 = 5.3.4 =
 
